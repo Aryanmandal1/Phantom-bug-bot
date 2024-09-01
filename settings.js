1cbb2dc2ce00c708a3e76c2037e6c57af7acbb17
ᎧᎮ ᭄ ★Aʀʏᴀɴ ★࿐ <157402412+Aryanmandal1@users.noreply.github.com>
2024-09-02 01:26:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,15 +8,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348108395270"
-global.ownername = "*Phantom+Hacker"
-global.ytname = "YT: phantom-hacks01"
+global.ownernomer = "918670755006"
+global.ownername = "*Aryan sir bot"
+global.ytname = "YT: aryan-sir"
 global.socialm = ""
 global.location = "India, Mizoram, Aizawl"
 
 global.ownernumber = '2348108395270'  //creator number
-global.ownername = 'Phantom+Hacker' //owner name
-global.botname = 'xbug bot' //name of the bot
+global.ownername = 'Aryan sir' //owner name
+global.botname = 'Aryan sir bot' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By Phantom'
@@ -27,7 +27,7 @@ global.themeemoji = '😡'
 global.wm = "Phantom"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaBROanEquiLW3ii1n47'
+global.link = 'https://whatsapp.com/channel/0029VaBROanEquiLW3ii1n4'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
@@ -35,7 +35,7 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
+global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
